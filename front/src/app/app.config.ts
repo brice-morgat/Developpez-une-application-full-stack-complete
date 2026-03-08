d1a4da92c4aec8370fd20c45e6e959607928edc8
brice.morgat <brice.morgat@gmx.fr>
2026-03-08 16:04:54 +0100

    chore(front): replace ngrx with ngxs

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,20 +1,14 @@
-import { isDevMode } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideStore } from '@ngxs/store';
 import { routes } from './app.routes';
-import { appReducer } from './core/state/app.state';
+import { AppState } from './core/state/app.state';
 
 export const appConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    provideStore({ app: appReducer }),
-    provideStoreDevtools({
-      maxAge: 25,
-      logOnly: !isDevMode(),
-    }),
+    provideStore([AppState]),
   ],
 };
 
